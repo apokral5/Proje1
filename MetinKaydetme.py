62c1931771858cb1981ccc0672aacdaa2ca73e31
apokral5 <137694525+apokral5@users.noreply.github.com>
2023-09-05 14:09:22 +0300

    Update MetinKaydetme.py
    Text File Project

--- MetinKaydetme.py
+++ MetinKaydetme.py
@@ -1,1 +1,8 @@
-
+import sys
+from PyQt5 import QtWidgets,QtGui
+def Pencere():
+  app=QtWidgets.QApplication(sys.argv)
+  self.yazi_alani=QtWidgets.QLineEdit()
+  self.temizle=QtWidgets.QPushButton("Temizle")
+  self.kaydet=QtWidgets.QPushButton("Kaydet")
+  v_box=QtWidgets.Q
